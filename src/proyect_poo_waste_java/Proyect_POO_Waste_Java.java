0c8f50e8d75841b21505ff76efd3a34426e6a06c
sergi <sergi@sergio>
2026-04-13 11:57:34 -0500

    Testeando cambios 2 dias repositorio

--- src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
+++ src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
@@ -4,6 +4,7 @@ public class Proyect_POO_Waste_Java {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("probando cambiosss");
+        System.out.println("dia dos testeando cambios,");
     }
     
 }
