c6625ee9e97a346534d059b439b72ca7724c5717
sergi <sergi@sergio>
2026-04-12 21:26:25 -0500

    probando cambios para el repositorio java

--- src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
+++ src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
@@ -3,7 +3,7 @@ package proyect_poo_waste_java;
 public class Proyect_POO_Waste_Java {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("probando cambios");
+        System.out.println("probando cambiosss");
     }
     
 }
