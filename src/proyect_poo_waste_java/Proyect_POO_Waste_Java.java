52225997df37adfd41df16fbe8ca48511e00408c
sergi <sergi@sergio>
2026-04-12 21:17:13 -0500

    hello world

--- src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
+++ src/proyect_poo_waste_java/Proyect_POO_Waste_Java.java
@@ -2,7 +2,7 @@
 package proyect_poo_waste_java;
 public class Proyect_POO_Waste_Java {
     public static void main(String[] args) {
-        
+        System.out.println("hello world");
     }
     
 }
