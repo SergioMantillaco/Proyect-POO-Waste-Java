5e2fd18f7e7bb76c1a4639cce66c329766535fcb
sergi <sergi@sergio.lan>
2026-05-24 16:32:22 -0500

    error de codigo en patron motor, intentando de nuevo

--- src/proyect_poo_waste_java/JuegoPrincipal.java
+++ src/proyect_poo_waste_java/JuegoPrincipal.java
@@ -1,19 +1,7 @@
-
 package proyect_poo_waste_java;
+
 public class JuegoPrincipal {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("probando cambiosss");
-        System.out.println("dia dos testeando cambios,");
-        System.out.println("test numero 3");
-        System.out.println("probando repositorio clonado");
-        System.out.println("probando cambios desde el repositorio de github");
-        System.out.println("probando rama");
-        System.out.println("probando");
-        System.out.println("prueba tutorial");
-        System.out.println("prueba de descarga");
-        System.out.println("prueba descarga 2");
-        System.out.println("otra vez prueba");
+        
     }
-    
 }
--- src/proyect_poo_waste_java/Motor.java
+++ src/proyect_poo_waste_java/Motor.java
@@ -1,26 +1,16 @@
 package proyect_poo_waste_java;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Motor implements PatronMotor {
-    
-    private ArrayList<Residuo> residuosEnPantalla;
-
-    public Motor() {
-        residuosEnPantalla = new ArrayList<>();
-    }
 
     @Override
     public void generarResiduo(Residuo r) {
-        residuosEnPantalla.add(r);
     }
 
     @Override
     public void actualizarFisicas(Velocidad_Por_Dificultad dificultad) {
-        for (int i = 0; i < residuosEnPantalla.size(); i++) {
-            Residuo actual = residuosEnPantalla.get(i);
-            actual.caer(dificultad);
-            System.out.println(actual); 
-        }
     }
+
+  
 }
