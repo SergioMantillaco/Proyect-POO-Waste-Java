30cb79f97083da2b0aa28ca10d8d526ef2408f71
sergi <sergi@sergio.lan>
2026-05-24 15:54:56 -0500

    En la clase abstracta Residuo:
    se actualize las variables de privadas a protegidas para que las clases hijas puedan modificar su valor
    
    se añadio el metodo abstracto caer el cual todas las clases hijas de residuo lo Deben tener
    
    se añadio un toString para hacer testeo

--- src/proyect_poo_waste_java/Residuo.java
+++ src/proyect_poo_waste_java/Residuo.java
@@ -1,18 +1,24 @@
 package proyect_poo_waste_java;
 public abstract class  Residuo {
     
-    private double posicionx;
-    private double posiciony;
-    private  double VelocidadBase = 1;
-   
+    protected double posicionx;
+    protected double posiciony;
+    protected double velocidadBase = 1.0;      
     public Residuo(double posicionx, double posiciony) {
         this.posicionx = posicionx;
         this.posiciony = posiciony;
     }
     
+    public abstract void caer(Velocidad_Por_Dificultad dificultadActual);
     
         // public abstract imagen a mostrar
    
     
+    //para que juan ardilla testee
+    @Override
+    public String toString() {
+        return "Residuo tipo: " + this.getClass().getSimpleName() + 
+               " | Posicion [X: " + posicionx + ", y: " + posiciony + "]";
+    }
     
 }
