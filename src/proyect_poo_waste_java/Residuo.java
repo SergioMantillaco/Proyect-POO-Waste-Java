423c27187913cfdee5d82ea4d9f65a30ba13ef07
sergi <sergi@sergio.lan>
2026-05-24 15:36:31 -0500

    Residuo abstract modificacion plantilla

--- src/proyect_poo_waste_java/Residuo.java
+++ src/proyect_poo_waste_java/Residuo.java
@@ -1,4 +1,18 @@
 package proyect_poo_waste_java;
-public class Residuo {
+public abstract class  Residuo {
+    
+    private double posicionx;
+    private double posiciony;
+    private  double VelocidadBase = 1;
+   
+    public Residuo(double posicionx, double posiciony) {
+        this.posicionx = posicionx;
+        this.posiciony = posiciony;
+    }
+    
+    
+        // public abstract imagen a mostrar
+   
+    
     
 }
